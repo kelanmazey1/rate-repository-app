2b38f5b4272ef582481c6402cb647bed8d973ee9
Kelan Mazey <kelanmazey1@gmail.com>
2020-12-04 21:04:08 +0000

    completed 10.11

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,13 +1,13 @@
-// import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const { loading, data, refetch } = useQuery(GET_REPOSITORIES, {
+  const { loading, data } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
   });
-  return { repositories: data.repositories, loading, refetch };
+
+  return { repositories: data?.repositories, loading };
 };
 
 export default useRepositories;
